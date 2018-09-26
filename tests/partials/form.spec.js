4aa314bdd49b135aeda28d30cb8d31786b56aa44
Liam Keaton <liamkeaton@gmail.com>
2018-09-26 16:29:40 +0100

    Form partial (#36)
    * Form partial
     🐿 v2.9.0
    
    * use right template
     🐿 v2.9.0

---
+++ tests/partials/form.spec.js
@@ -0,0 +1,48 @@
+const { expect } = require('chai');
+const {
+	fetchPartial,
+	registerPartial,
+	unregisterPartial
+} = require('../helpers');
+
+let context = {};
+
+describe('form template', () => {
+	before(async () => {
+		context.template = await fetchPartial('form.html');
+		registerPartial('fields', '<div></div>');
+	});
+
+	after(() => {
+		unregisterPartial('fields');
+	});
+
+	it('should have a POST method by default', () => {
+		const $ = context.template({});
+
+		expect($('form').attr('method')).to.equal('POST');
+	});
+
+	it('should respect the method passed in', () => {
+		const $ = context.template({
+			method: 'test'
+		});
+
+		expect($('form').attr('method')).to.equal('test');
+	});
+
+	it('should have an empty action by default', () => {
+		const $ = context.template({});
+
+		expect($('form').attr('action')).to.equal('');
+	});
+
+	it('should respect an action passed in', () => {
+		const $ = context.template({
+			action: '/test'
+		});
+
+		expect($('form').attr('action')).to.equal('/test');
+	});
+
+});
