6416bc633281c21bc6378282468237444ce7a7e7
Alex Naish <alex@naish.io>
2018-10-19 11:47:55 +0100

    Allow splitting countries by usage (#56)
    * duplicate frequent entries, allow filtering and splitting of results
    
    * update logic for handling edge cases
    
     🐿 v2.10.3

--- helpers/ncf-countries.js
+++ helpers/ncf-countries.js
@@ -1,11 +1,39 @@
 const { countries } = require('n-common-static-data').billingCountries;
 
+const MINIMUM_TO_SPLIT = 50;
+const MINIMUM_TO_SHOW_SPLIT = 2;
+
 module.exports = function ({ hash = {}, fn }) {
 	const data = Array.isArray(hash.filterList) ? countries.filter(countryInFilterList(hash.filterList)) : countries;
-	const context = Object.assign({ countries: data }, this);
+	const newContext = data.length >= MINIMUM_TO_SPLIT ? splitIntoUsage(data) : { countries: data };
+	const context = Object.assign(newContext, this);
 	return fn(context);
 };
 
 function countryInFilterList (filterList) {
 	return item => filterList.includes(item.code);
+}
+
+function splitIntoUsage (countries) {
+	const frequentCountries = ['GBR', 'USA', 'JPN', 'FRA', 'CAN'];
+	let foundFrequent = countries.map((item) => {
+		return frequentCountries.includes(item.code) && item;
+	}).filter(Boolean);
+
+	if (foundFrequent.length < MINIMUM_TO_SHOW_SPLIT) {
+		// Not worth showing the split version, just return the original
+		return { countries };
+	}
+
+	// Sort the frequently used countries into the usage metrics order (as defined on L#19)
+	foundFrequent = foundFrequent.sort((a, b) => {
+		return frequentCountries.indexOf(a.code) - frequentCountries.indexOf(b.code);
+	});
+
+	return {
+		ncfCountryGroups: [
+			{ label: 'Frequently Used', countries: foundFrequent },
+			{ label: 'Alphabetical', countries }
+		]
+	};
 }
--- tests/helpers/ncf-countries.spec.js
+++ tests/helpers/ncf-countries.spec.js
@@ -2,35 +2,82 @@ const sinon = require('sinon');
 const proxyquire = require('proxyquire').noCallThru();
 const { expect } = require('chai');
 
-const mockBillingCountries = [{ code: 'a'}, { code: 'b'}, { code: 'c'}, { code: 'd'}];
-const helper = proxyquire('../../helpers/ncf-countries', {
-	'n-common-static-data': {
-		billingCountries: { countries: mockBillingCountries }
-	}
-});
-
 describe('NCF Countries', () => {
 
+	let helper;
 	let stub;
 
 	beforeEach(() => {
 		stub = sinon.stub();
 	});
 
-	it('sets countries on the context', () => {
+	context('filterList', () => {
 
-		helper({ hash: {}, fn: stub });
-		const context = stub.getCall(0).args[0];
-		expect(context).to.have.property('countries', mockBillingCountries);
+		beforeEach(() => {
+			const mockBillingCountries = generateCountryArray(4, { includeAllFrequent: false });
+			helper = proxyquire('../../helpers/ncf-countries', {
+				'n-common-static-data': {
+					billingCountries: { countries: mockBillingCountries }
+				}
+			});
+		});
+
+		it('filters billingCountries', () => {
+			const filterList = ['C-0', 'C-2'];
+			helper({ hash: { filterList }, fn: stub });
+			const context = stub.getCall(0).args[0];
+			expect(context.countries).to.deep.equal([{ code: 'C-0'}, { code: 'C-2'}]);
+			expect(context.countries).to.not.include([{ code: 'C-1'}, { code: 'C-4'}]);
+		});
+
 	});
 
-	it('filterList filters billingCountries', () => {
-		const filterList = ['b', 'c'];
-		helper({ hash: { filterList }, fn: stub });
-		const context = stub.getCall(0).args[0];
-		expect(context.countries).to.deep.equal([{ code: 'b'}, { code: 'c'}]);
-		expect(context.countries).to.not.include([{ code: 'a'}, { code: 'd'}]);
+	context('when there are more than 50 results', () => {
+		let mockBillingCountries;
+		beforeEach(() => {
+			mockBillingCountries = generateCountryArray(75);
+			helper = proxyquire('../../helpers/ncf-countries', {
+				'n-common-static-data': {
+					billingCountries: { countries: mockBillingCountries }
+				}
+			});
+		});
+
+		it('splits the result set  on the context', () => {
+			helper({ hash: {}, fn: stub });
+			const context = stub.getCall(0).args[0];
+			expect(context).to.have.property('ncfCountryGroups');
+			expect(context.ncfCountryGroups.length).to.equal(2);
+
+			const [frequent, alphabetical ] = context.ncfCountryGroups;
+
+			expect(frequent.label).to.equal('Frequently Used');
+			expect(frequent.countries.map(c => c.code)).to.have.members(['GBR', 'USA', 'FRA', 'CAN', 'JPN']);
+			expect(alphabetical.label).to.equal('Alphabetical');
+			expect(alphabetical.countries).to.deep.equal(mockBillingCountries);
+		});
+
+		it('does not split when a filter means there are less than two frequent countries', () => {
+			// Get all country codes APART from 'CAN','FRA', 'JPN', 'USA' => only GBR would be returned
+			const notWanted = ['CAN','FRA', 'JPN', 'USA'];
+			const filterList = mockBillingCountries.filter(country => !notWanted.includes(country.code)).map(c => c.code);
+
+			helper({ hash: { filterList }, fn: stub });
+			const context = stub.getCall(0).args[0];
+			expect(context).to.not.have.property('ncfCountryGroups');
+			expect(context).to.have.property('countries');
+
+			expect(context.countries.map(c => c.code)).to.not.have.members(notWanted);
+
+		});
 
 	});
 
 });
+
+function generateCountryArray (length, { includeAllFrequent = true } = {}) {
+	return Array.from(Array(length), (item, index) => ({
+		code: `C-${index}`
+	}))
+	.concat(includeAllFrequent ? [{ code: 'GBR' }, { code: 'USA' }, { code: 'FRA' }, { code: 'CAN' }, { code: 'JPN' }] : []);
+}
