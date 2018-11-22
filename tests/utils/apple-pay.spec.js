3c751dbbed50f9aa7b7015723a7f929e95f84522
Liam Keaton <liamkeaton@gmail.com>
2018-11-22 16:08:39 +0000

    Lighter weight ApplePay utility (#70)
    * Payment utility with Apple Pay
     🐿 v2.10.3
    
    * SASS linting
     🐿 v2.10.3
    
    * Prenvent default on click and throw if Payment Request API not available
     🐿 v2.10.3
    
    * Adding hidding
     🐿 v2.10.3
    
    * Methods as an array
     🐿 v2.10.3
    
    * Fix test
     🐿 v2.10.3
    
    * Improving docs
     🐿 v2.10.3
    
    * Adding new ApplePay utility
     🐿 v2.10.3
    
    * Removing old payment utility
     🐿 v2.10.3
    
    * method -> methods
     🐿 v2.10.3
    
    * Custom error for browser support
     🐿 v2.10.3
    
    * Moving const's into getters
     🐿 v2.10.3
    
    * docblocks
     🐿 v2.10.3

---
+++ tests/utils/apple-pay.spec.js
@@ -0,0 +1,145 @@
+const ApplePay = require('../../utils/apple-pay');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+describe('Apple Pay', () => {
+	let request;
+	let window;
+	let event;
+	let sandbox;
+	let applePay;
+
+	beforeEach(() => {
+		request = { canMakePayment: function () {}, show: function () {} };
+		window = { PaymentRequest: function () { return request; }, fetch: function () { return Promise.resolve({ json: function () {} }); } };
+		event = { complete: function () {} };
+
+		sandbox = sinon.createSandbox();
+		sandbox.spy(request, 'canMakePayment');
+		sandbox.spy(request, 'show');
+		sandbox.spy(window, 'PaymentRequest');
+		sandbox.spy(window, 'fetch');
+		sandbox.spy(event, 'complete');
+	});
+
+	afterEach(() => {
+		sandbox.restore();
+	});
+
+	describe('public methods', () => {
+		describe('constructor', () => {
+			it('should throw an error if PaymentRequest not available', () => {
+				window.PaymentRequest = null;
+				expect(() => {
+					new ApplePay(window);
+				}).to.throw();
+			});
+		});
+
+		describe('canMakePayment', () => {
+			it('should proxy canMakePayment to the request', () => {
+				applePay = new ApplePay(window);
+				applePay.canMakePayment();
+				expect(request.canMakePayment.called).to.be.true;
+			});
+		});
+
+		describe('show', () => {
+			it('should proxy show to the request', () => {
+				applePay = new ApplePay(window);
+				applePay.show();
+				expect(request.show.called).to.be.true;
+			});
+
+			it('should not create new request if no details passed', () => {
+				applePay = new ApplePay(window);
+				applePay.show();
+				expect(window.PaymentRequest.callCount).to.equal(1);
+			});
+
+			it('should create new request if new details passed', () => {
+				applePay = new ApplePay(window);
+				applePay.show({ total: { label: 'new' }});
+				expect(window.PaymentRequest.callCount).to.equal(2);
+			});
+
+			it('should setup onmerchantvalidation method', () => {
+				delete request.onmerchantvalidation;
+				applePay = new ApplePay(window);
+				applePay.show();
+				expect(request.onmerchantvalidation).to.be.a('function');
+			});
+		});
+
+		describe('handleMerchantValidation', () => {
+			it('should use the production merchant validation URL by default', () => {
+				applePay = new ApplePay(window);
+				applePay.handleMerchantValidation(event);
+				expect(window.fetch.calledOnceWith(ApplePay.MERCHANT_VALIDATION_URL));
+			});
+
+			it('should use the test merchant validation URL', () => {
+				applePay = new ApplePay(window, ApplePay.TEST_PAYMENT_METHODS);
+				applePay.handleMerchantValidation(event);
+				expect(window.fetch.calledOnceWith(ApplePay.MERCHANT_VALIDATION_URL));
+			});
+
+			it('should call event.complete with response', async () => {
+				applePay = new ApplePay(window);
+				await applePay.handleMerchantValidation(event);
+				expect(event.complete.called).to.be.true;
+			});
+		});
+	});
+
+
+	describe('static methods', () => {
+		describe('getMerchantId', () => {
+			it('should return default merchant id', () => {
+				expect(ApplePay.getMerchantId()).to.be.a('string');
+			});
+
+			it('should return default merchant id if methods has no data', () => {
+				const methods = [{}];
+				expect(ApplePay.getMerchantId(methods)).to.be.a('string');
+			});
+
+			it('should return default merchant id if methods data hasn\'t got one', () => {
+				const methods = [{ data: {} }];
+				expect(ApplePay.getMerchantId(methods)).to.be.a('string');
+			});
+
+			it('should return merchant id from methods data', () => {
+				const methods = [{ data: { merchantIdentifier: 'test' } }];
+				expect(ApplePay.getMerchantId(methods)).to.equal('test');
+			});
+		});
+
+		describe('getMerchantValidationUrl', () => {
+			it('should return production merchant validation url', () => {
+				expect(ApplePay.getMerchantValidationUrl(ApplePay.MERCHANT_ID)).to.equal(ApplePay.MERCHANT_VALIDATION_URL);
+			});
+
+			it('should return a differnt merchant validation url for test merchant', () => {
+				expect(ApplePay.getMerchantValidationUrl(ApplePay.TEST_MERCHANT_ID)).to.equal(ApplePay.TEST_MERCHANT_VALIDATION_URL);
+			});
+
+			it('should default to the production merchant validation url', () => {
+				expect(ApplePay.getMerchantValidationUrl()).to.equal(ApplePay.MERCHANT_VALIDATION_URL);
+			});
+		});
+
+		describe('getPaymentDetails', () => {
+			it('should format given values in payment details object', () => {
+				const label = 'test';
+				const value = 1.01;
+				const currency = 'GBP';
+				const details = ApplePay.getPaymentDetails(value, currency, label);
+
+				expect(details.total.label).to.equal(label);
+				expect(Object.values(details.total.amount)).to.deep.include(value);
+				expect(Object.values(details.total.amount)).to.deep.include(currency);
+			});
+		});
+	});
+});
---
+++ utils/apple-pay.js
@@ -0,0 +1,239 @@
+/**
+ * Light weight wrapper around PaymentRequest API for ApplePay
+ * @example
+ * // Setting up production
+ * try {
+ *     const applePay = new ApplePay(window);
+ *     const canMakePayment = await applePay.canMakePayment();
+ *     if (canMakePayment) {
+ *         // Show payment button
+ *     }
+ * } catch (error) {
+ *     // Browser may not support it
+ * }
+ *
+ * // Use test by passing the test payment methods
+ * const applePay = new ApplePay(window, ApplePay.TEST_PAYMENT_METHODS);
+ *
+ * // On click of payment button you can updated payment details
+ * try {
+ *     const updatedPaymentDetails = ApplePay.getPaymentDetails(20.00, 'GBP', 'FT Standard');
+ *     const response = await applePay.show(updatedPaymentDetails);
+ *     // Send to Membership for fulfilment
+ *     if (success) {
+ *         response.complete('success');
+ *     } else {
+ *         response.complete('fail');
+ *     }
+ * } catch (error) {
+ *     // Payment failed
+ * }
+ */
+class ApplePay {
+	/**
+	 * Setup a Payment Request API request with Apple Pay defaults
+	 * @param {object} window Browser window object
+	 * @param {array} methods PaymentRequest API payment methods array
+	 * @param {object} details PaymentRequest API payment details object
+	 * @param {object} options PaymentRequest API payment options object
+	 * @throws If browser doesn't support PaymentRequest API
+	 */
+	constructor (window, methods = ApplePay.PAYMENT_METHODS, details = ApplePay.PAYMENT_DETAILS, options = ApplePay.PAYMENT_OPTIONS) {
+		if (!window.PaymentRequest) {
+			throw new Error('Browser does not support PaymentRequest API');
+		}
+
+		this.window = window;
+		this.methods = methods;
+		this.details = details;
+		this.options = options;
+		this.request = new this.window.PaymentRequest(this.method, this.details, this.options);
+	}
+
+	/**
+	 * Proxy call through to the request
+	 * @return {promise<boolean>}
+	 */
+	canMakePayment () {
+		return this.request.canMakePayment();
+	}
+
+	/**
+	 * Display the payment screen to the user
+	 * @param {object} paymentDetails Optional payment details object for update
+	 * @return {promise<object>} Response object from apple pay
+	 */
+	show (paymentDetails) {
+		// Work around to enable the update of payment details for safari
+		// browsers as it seems not to be supported, regenerates PaymentRequest
+		if (paymentDetails) {
+			this.details = paymentDetails;
+			this.request = new this.window.PaymentRequest(this.method, this.details, this.options);
+		}
+		this.request.onmerchantvalidation = event => this.handleMerchantValidation(event);
+		return this.request.show();
+	}
+
+	/**
+	 * Validate ourselves via membership
+	 * @param {object} event
+	 */
+	async handleMerchantValidation (event) {
+		const merchantId = ApplePay.getMerchantId(this.method);
+		const url = ApplePay.getMerchantValidationUrl(merchantId);
+		const data = {
+			validationUrl: event.validationURL,
+			displayName: 'FT.com',
+			domainName: 'www.ft.com',
+			merchantId
+		};
+		try {
+			const response = await this.window.fetch(url, {
+				method: 'POST',
+				mode: 'cors',
+				cache: 'no-cache',
+				headers: {
+					'Content-Type': 'application/json; charset=utf-8'
+				},
+				body: JSON.stringify(data)
+			});
+			return event.complete(response.json());
+		} catch (error) {
+			this.request.abort();
+			throw error;
+		}
+	}
+
+	/**
+	 * Return merchant id from methods or default to production id
+	 * @param {array} methods
+	 * @return {string}
+	 */
+	static getMerchantId (methods = []) {
+		const method = methods[0] || {};
+		return method.data && method.data.merchantIdentifier || ApplePay.MERCHANT_ID;
+	}
+
+	/**
+	 * Return merchant validation URL based on the merchant ID
+	 * @param {string} merchantId
+	 * @return {String}
+	 */
+	static getMerchantValidationUrl (merchantId) {
+		if (merchantId === ApplePay.TEST_MERCHANT_ID) {
+			return ApplePay.TEST_MERCHANT_VALIDATION_URL;
+		}
+		return ApplePay.MERCHANT_VALIDATION_URL;
+	}
+
+	/**
+	 * Helper method to format payment details correctly
+	 * @param {number} value Cost in 2 digit decimal number
+	 * @param {string} currency ISO 3 digit currency code
+	 * @param {string} label Product name
+	 */
+	static getPaymentDetails (value, currency, label) {
+		const details = ApplePay.PAYMENT_DETAILS;
+		details.total.label = label;
+		details.total.amount.value = value;
+		details.total.amount.currency = currency;
+		return details;
+	}
+
+	/**
+	 * Expose the production merchant id
+	 * @return {string}
+	 */
+	static get MERCHANT_ID () {
+		return 'merchant.com.ft';
+	}
+
+	/**
+	 * Expose the production merchant validation url
+	 * @return {string}
+	 */
+	static get MERCHANT_VALIDATION_URL () {
+		return 'https://api.ft.com/idm/v1/apple-merchant-validation/validate';
+	}
+
+	/**
+	 * Expose production payment methods
+	 * @return {string}
+	 */
+	static get PAYMENT_METHODS () {
+		return [{
+			supportedMethods: 'https://apple.com/apple-pay',
+			data: {
+				version: 1,
+				merchantIdentifier: ApplePay.MERCHANT_ID,
+				merchantCapabilities: ['supports3DS'],
+				supportedNetworks: ['amex', 'discover', 'masterCard', 'visa'],
+				countryCode: 'GB',
+			}
+		}];
+	}
+
+	/**
+	 * Default payment details
+	 * @return {object}
+	 */
+	static get PAYMENT_DETAILS () {
+		return {
+			total: {
+				label: 'FT.com',
+				amount: {
+					value: 0.01,
+					currency: 'GBP'
+				},
+			}
+		};
+	};
+
+	/**
+	 * Default payment options
+	 * @return {object}
+	 */
+	static get PAYMENT_OPTIONS () {
+		return {
+			requestPayerName: false,
+			requestPayerEmail: false,
+			requestPayerPhone: false,
+			requestShipping: false
+		};
+	}
+
+	/**
+	 * Expose the test merchant id
+	 * @return {string}
+	 */
+	static get TEST_MERCHANT_ID () {
+		return 'merchant.test.env.apple.pay';
+	}
+
+	/**
+	 * Expose the test merchant validation url
+	 * @return {string}
+	 */
+	static get TEST_MERCHANT_VALIDATION_URL () {
+		return 'https://api-t.ft.com/idm/v1/apple-merchant-validation/validate';
+	}
+
+	/**
+	 * Expose the test payment methods
+	 * @return {string}
+	 */
+	static get TEST_PAYMENT_METHODS () {
+		return [{
+			supportedMethods: 'https://apple.com/apple-pay',
+			data: {
+				version: 1,
+				merchantIdentifier: ApplePay.TEST_MERCHANT_ID,
+				merchantCapabilities: ['supports3DS'],
+				supportedNetworks: ['amex', 'discover', 'masterCard', 'visa'],
+				countryCode: 'GB',
+			}
+		}];
+	}
+}
+
+module.exports = ApplePay;
