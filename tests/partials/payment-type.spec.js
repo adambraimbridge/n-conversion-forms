0ae3ebcf3a11f1b8ff5389b46d52f97f8b75e6c0
Liam Keaton <liamkeaton@gmail.com>
2019-02-06 15:15:40 +0000

    Enable and disable payment types from showing (#84)
    🐿 v2.12.0

--- tests/partials/payment-type.spec.js
+++ tests/partials/payment-type.spec.js
@@ -4,47 +4,98 @@ const {
 } = require('../helpers');
 
 let context = {};
+let params;
 
 describe('payment-type', () => {
 	before(async () => {
 		context.template = await fetchPartial('payment-type.html');
+		params = {
+			enableCreditcard: true,
+			enableDirectdebit: true,
+			enablePaypal: true,
+			enableApplepay: true
+		};
 	});
 
 	it('should default to no payment type selected', () => {
-		const $ = context.template({});
-
+		const $ = context.template(params);
 		expect($('input[checked]').length).to.equal(0);
 	});
 
 	it('should default to the given value of creditcard', () => {
-		const value = 'creditcard';
-		const $ = context.template({
-			value
-		});
-		expect($('input[checked]').attr('value')).to.equal(value);
+		params.value = 'creditcard';
+		const $ = context.template(params);
+		expect($('input[checked]').attr('value')).to.equal(params.value);
 	});
 
 	it('should default to the given value of directdebit', () => {
-		const value = 'directdebit';
-		const $ = context.template({
-			value
-		});
-		expect($('input[checked]').attr('value')).to.equal(value);
+		params.value = 'directdebit';
+		const $ = context.template(params);
+		expect($('input[checked]').attr('value')).to.equal(params.value);
 	});
 
 	it('should default to the given value of paypal', () => {
-		const value = 'paypal';
-		const $ = context.template({
-			value
-		});
-		expect($('input[checked]').attr('value')).to.equal(value);
+		params.value = 'paypal';
+		const $ = context.template(params);
+		expect($('input[checked]').attr('value')).to.equal(params.value);
 	});
 
 	it('should default to the given value of applepay', () => {
-		const value = 'applepay';
-		const $ = context.template({
-			value
+		params.value = 'applepay';
+		const $ = context.template(params);
+		expect($('input[checked]').attr('value')).to.equal(params.value);
+	});
+
+	it('should show no payment options by default', () => {
+		expectPaymentType(context);
+	});
+
+	it('should show only creditcard', () => {
+		expectPaymentType(context, { creditcard: true });
+	});
+
+	it('should show only directdebit', () => {
+		expectPaymentType(context, { directdebit: true });
+	});
+
+	it('should show only paypal', () => {
+		expectPaymentType(context, { paypal: true });
+	});
+
+	it('should show only applepay', () => {
+		expectPaymentType(context, { applepay: true });
+	});
+
+	it('should show creditcard and applepay', () => {
+		expectPaymentType(context, {
+			creditcard: true,
+			applepay: true
 		});
-		expect($('input[checked]').attr('value')).to.equal(value);
+	});
+
+	it('should show all payment types', () => {
+		expectPaymentType(context, {
+			creditcard: true,
+			directdebit: true,
+			paypal: true,
+			applepay: true
+		});
 	});
 });
+
+function expectPaymentType (context, { creditcard=false, directdebit=false, paypal=false, applepay=false }={}) {
+	const $ = context.template({
+		enableCreditcard: creditcard,
+		enableDirectdebit: directdebit,
+		enablePaypal: paypal,
+		enableApplepay: applepay
+	});
+	expect($('input[value="creditcard"]').length === 1).to.equal(creditcard);
+	expect($('label[for="creditcard"]').length === 1).to.equal(creditcard);
+	expect($('input[value="directdebit"]').length === 1).to.equal(directdebit);
+	expect($('label[for="directdebit"]').length === 1).to.equal(directdebit);
+	expect($('input[value="paypal"]').length === 1).to.equal(paypal);
+	expect($('label[for="paypal"]').length === 1).to.equal(paypal);
+	expect($('input[value="applepay"]').length === 1).to.equal(applepay);
+	expect($('label[for="applepay"]').length === 1).to.equal(applepay);
+}
