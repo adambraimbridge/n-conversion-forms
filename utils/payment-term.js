dad22b21fcbefbdb642f82a70f7108ee0e2620bd
Liam Keaton <liamkeaton@gmail.com>
2019-02-13 16:35:09 +0000

    Add updateOptions to payment term utility  🐿 v2.12.0

--- utils/payment-term.js
+++ utils/payment-term.js
@@ -9,6 +9,10 @@
  * // Hide payment type
  * paymentType.hide(PaymentType.CREDITCARD);
  */
+
+const LABEL_TITLE_CLASS = '.ncf__payment-term__label--title';
+const LABEL_DESCRIPTION_CLASS = '.ncf__payment-term__label--description';
+
 class PaymentTerm {
 	/**
 	 * Initalise the PaymentTerm utility
@@ -40,6 +44,41 @@ class PaymentTerm {
 			throw new Error('No payment term has been selected');
 		}
 		return checked.getAttribute('value');
+	}
+
+	/**
+	 * Update the payment term options
+	 * @param {Array} options Array of objects contain terms information
+	 */
+	updateOptions (options) {
+		const selected = this.getSelected();
+		const inputToCopy = this.$paymentTerm.querySelector('input');
+		const labelToCopy = this.$paymentTerm.querySelector('label');
+		const container = inputToCopy.parentElement;
+
+		// Reduce to create an array of new input and label elements
+		const newElements = options.reduce((acc, option) => {
+			const input = inputToCopy.cloneNode();
+			input.setAttribute('id', option.value);
+			input.setAttribute('value', option.value);
+			input.checked = option.value === selected;
+
+			const label = labelToCopy.cloneNode(true);
+			label.setAttribute('for', option.value);
+			label.querySelector(LABEL_TITLE_CLASS).innerText = option.name;
+			label.querySelector(LABEL_DESCRIPTION_CLASS).innerHTML = option.description;
+
+			return acc.concat([input, label]);
+		}, []);
+
+		// Remove existing
+		this.$paymentTerm
+			.querySelectorAll('input,label')
+			.forEach(element => element.remove());
+
+		// Add new elements
+		newElements
+			.forEach(element => container.insertBefore(element, container.lastElementChild));
 	}
 }
 
