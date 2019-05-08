9a9be152f96f4cbca0ebbad685be2b4590995add
Liam Keaton <liamkeaton@gmail.com>
2019-05-08 15:17:56 +0100

    Add option to render zip code  🐿 v2.12.3

--- tests/partials/post-code.spec.js
+++ tests/partials/post-code.spec.js
@@ -1,3 +1,5 @@
+const { expect } = require('chai');
+
 const {
 	fetchPartial,
 	shouldBeDisableable,
@@ -11,6 +13,18 @@ let context = {};
 describe('postcode template', () => {
 	before(async () => {
 		context.template = await fetchPartial('postcode.html');
+	});
+
+	it('should be post code by default', () => {
+		const $ = context.template({});
+		expect($('label').text()).to.contain('Post code');
+	});
+
+	it('should render zip code if asked', () => {
+		const $ = context.template({
+			isZipCode: true
+		});
+		expect($('label').text()).to.contain('Zip code');
 	});
 
 	shouldPopulateValue(context);
