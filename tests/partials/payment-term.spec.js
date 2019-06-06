d884c2a7540fab79e16e5c0780db48a4f20bb918
Liam Keaton <liamkeaton@gmail.com>
2019-06-06 16:13:27 +0100

    Add the discount label for payment terms  🐿 v2.12.3

--- tests/partials/payment-term.spec.js
+++ tests/partials/payment-term.spec.js
@@ -28,19 +28,19 @@ describe('payment-term', () => {
 	it('should populate the name', () => {
 		const name = 'Test';
 		const $ = context.template({ options: [{ name }]});
-		expect($('.ncf__payment-term__label--title').text()).to.equal(name);
+		expect($('.ncf__payment-term__title').text()).to.equal(name);
 	});
 
 	it('should populate the description', () => {
 		const description = 'Test';
 		const $ = context.template({ options: [{ description }]});
-		expect($('.ncf__payment-term__label--description').text()).to.contain(description);
+		expect($('.ncf__payment-term__description').text()).to.contain(description);
 	});
 
 	it('should allow HTML in the description', () => {
 		const description = 'Test with an <a>anchor</a>';
 		const $ = context.template({ options: [{ description }]});
-		expect($('.ncf__payment-term__label--description a').length).to.equal(1);
+		expect($('.ncf__payment-term__description a').length).to.equal(1);
 	});
 
 	it('should populate the value', () => {
@@ -56,5 +56,19 @@ describe('payment-term', () => {
 		const option2 = { value: 'option2', selected: true };
 		const $ = context.template({ options: [option1, option2]});
 		expect($('input[checked]').attr('value')).to.equal(option2.value);
+	});
+
+	it('should not have discount text by default', () => {
+		const option1 = { value: 'option1' };
+		const option2 = { value: 'option2', selected: true };
+		const $ = context.template({ options: [option1, option2]});
+		expect($('.ncf__payment-term__discount').length).to.equal(0);
+	});
+
+	it('should have discount text if a discount is passed', () => {
+		const option1 = { value: 'option1', discount: '25%' };
+		const option2 = { value: 'option2', selected: true };
+		const $ = context.template({ options: [option1, option2]});
+		expect($('.ncf__payment-term__discount').length).to.equal(1);
 	});
 });
