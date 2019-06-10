fc17d72a4ed4904756f349377ae72fa81b6be399
Liam Keaton <liamkeaton@gmail.com>
2019-06-10 15:36:33 +0100

    Add payment term display to the package-change component  🐿 v2.12.3

--- tests/partials/package-change.spec.js
+++ tests/partials/package-change.spec.js
@@ -4,6 +4,11 @@ const {
 const expect = require('chai').expect;
 
 let context = {};
+const CONTENT_SELECTOR = '.ncf__package-change__content';
+const TITLE_SELECTOR = '.ncf__package-change__title';
+const DESCRIPTION_SELECTOR = '.ncf__package-change__description';
+const ANNUAL_COPY_SELECTOR = '.ncf__package-change__annual-copy';
+const DISCOUNT_SELECTOR = '.ncf__package-change__discount';
 
 describe('package-change template', () => {
 	before(async () => {
@@ -21,6 +26,120 @@ describe('package-change template', () => {
 		const data = { currentPackage: 'Digital' };
 		const $ = context.template(data);
 
-		expect($('.ncf__center').text()).to.contain(data.currentPackage);
+		expect($(CONTENT_SELECTOR).text()).to.contain(data.currentPackage);
+	});
+
+	describe('trial', () => {
+		it('should show the correct title copy', () => {
+			const name = 'trial';
+			const $ = context.template({ terms: [{
+				name
+			}]});
+			expect($(TITLE_SELECTOR).text()).to.equal('Try the FT');
+		});
+
+		it('should show the trial price', () => {
+			const name = 'trial';
+			const price = '£1.01';
+			const trialPrice = '£2.01';
+			const $ = context.template({ terms: [{
+				name,
+				price,
+				trialPrice
+			}]});
+			expect($(DESCRIPTION_SELECTOR).text()).to.contain(trialPrice);
+		});
+	});
+
+	describe('annual', () => {
+		it('should show the correct title copy', () => {
+			const name = 'annual';
+			const $ = context.template({ terms: [{
+				name
+			}]});
+			expect($(TITLE_SELECTOR).text()).to.contain('annually');
+		});
+
+		it('should show the price', () => {
+			const name = 'annual';
+			const price = '£1.01';
+			const $ = context.template({ terms: [{
+				name,
+				price
+			}]});
+			expect($(DESCRIPTION_SELECTOR).text()).to.contain(price);
+		});
+
+		it('should show discount copy if not discounted', () => {
+			const name = 'annual';
+			const $ = context.template({ terms: [{
+				name
+			}]});
+			expect($(ANNUAL_COPY_SELECTOR).length).to.equal(1);
+		});
+
+		it('should not show discount copy if discounted', () => {
+			const name = 'annual';
+			const discount = '25%';
+			const $ = context.template({ terms: [{
+				name,
+				discount
+			}]});
+			expect($(ANNUAL_COPY_SELECTOR).length).to.not.equal(1);
+		});
+	});
+
+	describe('quarterly', () => {
+		it('should show the correct title copy', () => {
+			const name = 'quarterly';
+			const $ = context.template({ terms: [{
+				name
+			}]});
+			expect($(TITLE_SELECTOR).text()).to.contain('quarterly');
+		});
+
+		it('should show the price', () => {
+			const name = 'quarterly';
+			const price = '£1.01';
+			const $ = context.template({ terms: [{
+				name,
+				price
+			}]});
+			expect($(DESCRIPTION_SELECTOR).text()).to.contain(price);
+		});
+	});
+
+	describe('monthly', () => {
+		it('should show the correct title copy', () => {
+			const name = 'monthly';
+			const $ = context.template({ terms: [{
+				name
+			}]});
+			expect($(TITLE_SELECTOR).text()).to.contain('monthly');
+		});
+
+		it('should show the price', () => {
+			const name = 'monthly';
+			const price = '£1.01';
+			const $ = context.template({ terms: [{
+				name,
+				price
+			}]});
+			expect($(DESCRIPTION_SELECTOR).text()).to.contain(price);
+		});
+	});
+
+	it('should not have discount text by default', () => {
+		const option1 = { value: 'option1' };
+		const option2 = { value: 'option2', selected: true };
+		const $ = context.template({ terms: [option1, option2]});
+		expect($(DISCOUNT_SELECTOR).length).to.equal(0);
+	});
+
+	it('should have discount text if a discount is passed', () => {
+		const option1 = { value: 'option1', discount: '25%' };
+		const option2 = { value: 'option2', selected: true };
+		const $ = context.template({ terms: [option1, option2]});
+		expect($(DISCOUNT_SELECTOR).length).to.equal(1);
 	});
 });
