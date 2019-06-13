e0697b723047fc11c131d9f0859ead017e812040
umbobabo <umbobabo77@gmail.com>
2019-06-13 15:47:48 +0100

    Example of visual test  🐿 v2.12.3

--- tests/visual/index.js
+++ tests/visual/index.js
@@ -3,20 +3,20 @@ const differencify = new Differencify();
 
 
 (async () => {
-  await differencify
-    .init()
-    .launch({
+	await differencify
+		.init()
+		.launch({
 		headless: false
 	})
-    .newPage()
-    .setViewport({ width: 1600, height: 1200 })
-    .goto('http://localhost:5005')
-    .waitFor(1000)
-    .screenshot()
-    .toMatchSnapshot()
-    .result((result) => {
-      console.log(result); // Prints true or false
-    })
-    .close()
-    .end();
+		.newPage()
+		.setViewport({ width: 1600, height: 1200 })
+		.goto('http://localhost:5005')
+		.waitFor(1000)
+		.screenshot()
+		.toMatchSnapshot()
+		.result((result) => {
+			console.log(result); // Prints true or false
+		})
+		.close()
+		.end();
 })();
