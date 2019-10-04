521648d1fe618db28be8221bb93afeb783497c10
umbobabo <umbobabo77@gmail.com>
2019-10-04 20:48:43 +0100

    refactor: better documentation  🐿 v2.12.4

--- test/helpers/expect-to-render-as.js
+++ test/helpers/expect-to-render-as.js
@@ -4,23 +4,23 @@ import toDiffableHtml from 'diffable-html';
 
 export const expectToRenderAs = {
 	toRenderAs (reactComponent, context, props) {
+		// Render the React component with props.
+		const ReactComponentRendered = ReactDOMServer.renderToStaticMarkup(reactComponent(props));
+		// Render with the same proprieties but using Handlebars.
+		const handlerBarsVersion = context.template(props);
 		// `this.isNot` indicates whether the assertion was inverted with `.not`
 		// which needs to be respected, otherwise it fails incorrectly.
-		// Render the react component.
-		const ReactComponentRendered = ReactDOMServer.renderToStaticMarkup(reactComponent(props));
-		// Rendere with the same proprieties but using Handlebars.
-		const handlerBarsVersion = context.template(props);
 		if (this.isNot) {
 			expect(toDiffableHtml(ReactComponentRendered)).notToBe(toDiffableHtml(handlerBarsVersion));
 		} else {
-			// Compare the 2 rendered version to be sure they match.
+			// Compare the 2 rendered versions to be sure they match and compatibility is preserved.
 			expect(toDiffableHtml(ReactComponentRendered)).toBe(toDiffableHtml(handlerBarsVersion));
 		}
 
 		expect(ReactComponentRendered).toMatchSnapshot();
 		expect(handlerBarsVersion).toMatchSnapshot();
 
-		// This point is reached when the above assertion was successful.
+		// This point is reached when the above assertions were successful.
 		// The test should therefore always pass, that means it needs to be
 		// `true` when used normally, and `false` when `.not` was used.
 		return {
