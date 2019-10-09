3c3de130ce7c5eff4bdc69e9b0b5b6610681ded0
dapenguin <anthony.jeffery@littleblueweb.com>
2019-10-09 20:19:34 +0100

    feat: phone component using JSX and tests

---
+++ components/phone.spec.js
@@ -0,0 +1,49 @@
+import Phone from './phone';
+import { expectToRenderAs } from '../test-jest/helpers/expect-to-render-as';
+import { fetchPartialAsString } from '../test-jest/helpers/fetch-hbs-as-string';
+
+const context = {
+};
+
+expect.extend(expectToRenderAs);
+
+describe('Phone', () => {
+	beforeAll(async () => {
+		context.template = await fetchPartialAsString('phone.html');
+	});
+
+	it('render a phone input with a label', () => {
+		const props = {
+			pattern: 'whatever',
+		};
+
+		expect(Phone).toRenderAs(context, props);
+	});
+
+	it('render a phone input with a label for B2B', () => {
+		const props = {
+			pattern: 'whatever',
+			isB2b: true,
+		};
+
+		expect(Phone).toRenderAs(context, props);
+	});
+
+	it('render a disabled phone input', () => {
+		const props = {
+			pattern: 'whatever',
+			isDisabled: true,
+		};
+
+		expect(Phone).toRenderAs(context, props);
+	});
+
+	it('render a phone input with error styling', () => {
+		const props = {
+			pattern: 'whatever',
+			hasError: true,
+		};
+
+		expect(Phone).toRenderAs(context, props);
+	});
+});
