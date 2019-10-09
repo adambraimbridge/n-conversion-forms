4fd730bd86d572e74e71be8bb1b3768dc1f097d6
ludrob <robin.ludovic@gmail.com>
2019-10-09 12:06:38 +0100

    add delivery-postcode  🐿 v2.12.4

---
+++ components/delivery-postcode.spec.js
@@ -0,0 +1,46 @@
+import DeliveryPostcode from './delivery-postcode';
+import { expectToRenderAs } from '../test-jest/helpers/expect-to-render-as';
+import { fetchPartialAsString } from '../test-jest/helpers/fetch-hbs-as-string';
+
+const context = {
+};
+
+expect.extend(expectToRenderAs);
+
+describe('Delivery Postcode', () => {
+	beforeAll(async () => {
+		context.template = await fetchPartialAsString('delivery-postcode.html');
+	});
+
+	it('render a postcode input with a label', () => {
+		const props = {
+			postcodeReference: 'ZipCode',
+			pattern: 'whatever',
+		};
+
+		expect(DeliveryPostcode).toRenderAs(context, props);
+
+	});
+
+	it('render a disable input', () => {
+		const props = {
+			postcodeReference: 'ZipCode',
+			pattern: 'whatever',
+			isDisabled: true,
+		};
+
+		expect(DeliveryPostcode).toRenderAs(context, props);
+
+	});
+
+	it('render different styles', () => {
+		const props = {
+			postcodeReference: 'ZipCode',
+			pattern: 'whatever',
+			hasError: true,
+			isHidden: true,
+		};
+
+		expect(DeliveryPostcode).toRenderAs(context, props);
+	});
+});
