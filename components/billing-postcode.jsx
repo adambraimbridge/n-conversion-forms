abc8ba89e8b0716d26eda1ff13df2b7a92e469c1
umbobabo <umbobabo77@gmail.com>
2019-10-14 15:20:07 +0100

    refactor: introduce backward compabitile but overridable ids and names, neater tests  🐿 v2.12.4

--- components/billing-postcode.jsx
+++ components/billing-postcode.jsx
@@ -8,7 +8,15 @@ export default function BillingPostcode ({
 	pattern = null,
 	isDisabled = false,
 	hasError = false,
-	isHidden = false }) {
+	isHidden = false,
+	fieldId = 'billingPostcodeField',
+	// This is clearly wrong but has been implemented this way in HBS.
+	// We will need to get rid of these fields anyway:
+	// https://github.com/Financial-Times/n-conversion-forms/issues/263
+	fieldName = 'deliveryPostcode',
+	inputId = 'billingPostcode',
+	inputName = 'billingPostcode',
+}) {
 
 	const BillingPostcodeFieldClassNames = classNames([
 		'o-forms o-forms--wide ncf__field js-field',
@@ -19,20 +27,20 @@ export default function BillingPostcode ({
 	]);
 
 	return (<div
-		id="billingPostcodeField"
+		id={fieldId}
 		className={BillingPostcodeFieldClassNames}
 		data-ui-item="form-field"
-		data-ui-item-name="deliveryPostcode"
+		data-ui-item-name={fieldName}
 		data-validate="required"
 	>
 
-		<label htmlFor="billingPostcode" className="o-forms__label">
+		<label htmlFor={inputId} className="o-forms__label">
 			Billing <span data-reference="postcode">{postcodeReference}</span>
 		</label>
 
 		<input type="text"
-			id="billingPostcode"
-			name="billingPostcode"
+			id={inputId}
+			name={inputName}
 			defaultValue={`${value}`}
 			placeholder={`Enter your ${postcodeReference}`}
 			autoComplete="postal-code"
@@ -57,5 +65,9 @@ BillingPostcode.propTypes = {
 	pattern: PropTypes.string,
 	isDisabled: PropTypes.bool,
 	hasError: PropTypes.bool,
-	isHidden: PropTypes.bool
+	isHidden: PropTypes.bool,
+	fieldId: PropTypes.string,
+	fieldName: PropTypes.string,
+	inputId: PropTypes.string,
+	inputName: PropTypes.string,
 };
