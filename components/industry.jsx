7d01a38d6783eb19374834614aeed05ccaf77d74
ludrob <robin.ludovic@gmail.com>
2019-11-15 12:00:52 +0000

    fix proptype imports 🐿 v2.12.5

--- components/industry.jsx
+++ components/industry.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { demographics } from 'n-common-static-data';
 const defaultOptions = demographics.industries.industries;
