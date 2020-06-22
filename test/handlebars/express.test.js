4459479061a87c1d00a2da1a88a20950c23ee2be
estefania morton <estefaniamorton@estefanias-MacBook-Pro.local>
2020-06-22 16:37:26 -0500

    added tests for handlebars 🐿 v2.13.0

---
+++ test/handlebars/express.test.js
@@ -0,0 +1,51 @@
+/*global it, describe, before*/
+'use strict';
+
+const request = require('supertest');
+const app = require('../fixtures/app/main');
+
+describe('express handlebars setup', function () {
+	before(function () {
+		return app.promise;
+	});
+
+	it('should do templating', function (done) {
+		request(app)
+			.get('/templated')
+			.expect(200, /FT/, done);
+	});
+
+	it('should not inherit any markup by default', function (done) {
+		request(app)
+			.get('/templated')
+			.expect(200, /^<h1>FT - on/, done);
+	});
+
+	it('should support loading partials via bower', function (done) {
+		request(app)
+			.get('/templated')
+			.expect(200, /End of dep 2 partial/, done);
+	});
+
+	it('should treat undefined flags as offy (like falsey)', function (done) {
+		request(app)
+			.get('/templated')
+			.expect(200, /<undefinedflag-on><\/undefinedflag-on>/, done);
+	});
+
+	describe('content helpers', function () {
+		it('should provide an if equals helper', function (done) {
+			request(app)
+				.get('/templated')
+				.expect(200, /ifEquals\:first not second/, done);
+		});
+
+		it('should provide an if some helper', function (done) {
+			request(app)
+				.get('/templated')
+				.expect(200, /ifSome\:first not second/, done);
+		});
+	});
+
+
+});
